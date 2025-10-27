82649940cfaea62d5b39131b29b51b19523bd98b
jaime <jaimelapluie13@gmail.com>
2025-10-27 15:47:53 +0500

    fix(bot): Handle CallbackQuery safely and redirect to original command after auth
    - Fixed AuthMiddleware to safely handle CallbackQuery, avoiding AttributeError for event.text
    - Implemented redirection to original command in handle_login_and_save_token using command_map for direct handler invocation
    - Added storage of original command in state.update_data to enable redirection

--- tg_bot/middlewares/auth.py
+++ tg_bot/middlewares/auth.py
@@ -6,6 +6,9 @@ from tg_bot.services.api_users import get_existing_token
 from tg_bot.states.profile import AuthStates
 
 
+reserved_commands_list = ('/start', '/profile', '/help')
+
+
 class AuthMiddleware(BaseMiddleware):
     """
     Перехватывает выполнение хэндлеров, получает токен из локальной базы
@@ -17,22 +20,35 @@ class AuthMiddleware(BaseMiddleware):
                        event: Message | CallbackQuery,
                        data: Dict[str, Any]
                        ) -> Any:
-        print("\n\nAuthMiddleware в работе")
+        print(f"\n\nAuthMiddleware в работе {"*" * 50}")
         telegram_id = event.from_user.id
         state = data['state']
         current_state = await state.get_state()
-        
-        if current_state == "AuthStates:waiting_for_password":
-            return await handler(event, data)  # Вызов auth хэндлера, чтобы не попасть в loop
-        
+
+        # Пропускаю зарезервированные команды, callback и состояние ожидания пароля
+        if (
+            isinstance(event, CallbackQuery)
+            or (isinstance(event, Message) and event.text in reserved_commands_list)
+            or current_state == "AuthStates:waiting_for_password"
+        ):
+            return await handler(event, data)  # Вызов исходного хэндлера без пароля
+
         # запрос токена из локальной БД
         is_token_exist, access_token = await get_existing_token(telegram_id)
         
         if is_token_exist:
             data['token'] = access_token
             return await handler(event, data)  # Вызов исходного хэндлера
-        
-        else:
+
+        # токена нет и состояние не AuthStates:waiting_for_password
+        if current_state != "AuthStates:waiting_for_password":
             await state.set_state(AuthStates.waiting_for_password)
-            await event.answer('Пожалуйста, введите пароль')
-            return
+            await event.answer('Пожалуйста, введите пароль!')
+            await state.update_data(original_command=event.text)
+
+"""
+fix(bot): Add callback handle, return to original handler from middleware
+
+- Implement callback at AuthMiddleware: push bots' button handle without error
+- Ensure logic to seamless return to original handler after correct password input
+"""
--- tg_bot/services/auth_service.py
+++ tg_bot/services/auth_service.py
@@ -1,19 +1,45 @@
-from aiogram import F, BaseMiddleware, Router
+from aiogram import F, Router
+
 from aiogram.types import Message, CallbackQuery
 from typing import Callable, Dict, Any, Awaitable
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+from tg_bot.config import bot_instance
+from tg_bot.handlers.words.listing import handle_view_words
+from tg_bot.handlers.words.new_adding import handle_add_word_command
 from tg_bot.keyboards.words_kb import check_data_kb
+
 from tg_bot.services.api_users import get_profile_by_telegram_id, get_token, put_token_to_local_db, get_existing_token
 from tg_bot.states.profile import AuthStates
-from tg_bot.states.words import AddWordProces
-
 
 auth_router = Router()
 
+command_map = {
+                '/view_words': {
+                    'handler': handle_view_words,
+                    'prompt': 'Возобновляю показ слов'},
+                '/add_word': {
+                    'handler': handle_add_word_command,
+                    'prompt': 'Возобновляю процесс добавления слова'},
+                '/srs': {
+                    'handler': 'RESERVED',  # TODO: добавить когда напишу
+                    'prompt': 'srs_reserved'},
+                
+                }
 
-@auth_router.message(AuthStates.waiting_for_password, ~F.text.startswith('/'))
+
+@auth_router.message(AuthStates.waiting_for_password, F.text.startswith('/'))
+async def cancel_auth_on_command(message: Message, state: FSMContext):
+    """
+    Хэндлер сбрасывает процесс ожидания пароля в случае ввода другой команды
+    """
+    print('\ncancel_auth_on_command Ввод пароля прерван другой командой')
+    await state.clear()
+    await message.answer(f'Ввод пароля прерван командой {message.text}')
+
+
+@auth_router.message(AuthStates.waiting_for_password, ~F.text.startswith('/'))  # ~F.text.startswith('/')
 async def handle_login_and_save_token(message: Message, state: FSMContext):
     """
     Ожидает пароль, пробует сделать запрос к API и получить токен и сохраняет его в
@@ -21,9 +47,13 @@ async def handle_login_and_save_token(message: Message, state: FSMContext):
     """
     
     print('\n>Отрабатывает service -> handle_login_and_save_token..')
+    data = await state.get_data()
+    original_handler = data.get('original_handler')
+    print(f"original_handler = {original_handler}")
     
     telegram_id = message.from_user.id
     password = message.text
+    print(f"password = {password}")
     
     # Попытка получения профиля по telegram_id
     is_exist, data = await get_profile_by_telegram_id(telegram_id=telegram_id)
@@ -40,25 +70,30 @@ async def handle_login_and_save_token(message: Message, state: FSMContext):
 
     if is_success:
         print(f"auth_token: {auth_token}")
-        await message.answer(f"Пароль верный. Успешно получены токены")
+        await message.answer(f"Пароль верный")
         user_data.pop("password")
         user_data.update(telegram_id=telegram_id)
         
         # Попытка сохранение токена в локальную БД
         is_done = await put_token_to_local_db(auth_token, user_data)
         if is_done:
-            await state.clear()
-            await message.answer('Теперь можно вернуться к первоначальному действию')
-            # TODO: не нужны ли тут breadcrumbs? Возврат к первоначальному хэндлеру, или он сам?
-            ##################################
-            # print("Врубаю AddWordProces.waiting_check_state")
-            #
-            # # Возврат к состоянию
-            # await state.set_state(AddWordProces.waiting_check_state)
-            # print("Врубил <<")
-            # await message.answer(text=f"Отлично, теперь вернемся к подтверждению действия по добавлению слова:",
-            #                      reply_markup=check_data_kb())
-            #############################################
+            # TODO: Возврат к первоначальному хэндлеру через feed_raw_update?
+            # Извлекаем сохранённую команду
+            data = await state.get_data()
+            original_command = data.get('original_command')
+            
+            await state.clear()  # Очищаем состояние после авторизации
+            
+            if original_command and not original_command.startswith('/'):
+                await message.answer('Теперь можете повторить команду')
+                return
+                
+            if original_command:
+                command_config = command_map.get(original_command)
+                original_handler = command_config.get('handler')
+                prompt = command_config.get('prompt')
+                await message.answer(prompt)
+                await original_handler(message, state)
             
         else:
             await message.answer(text='В handle_login_and_save_token произошла ошибка. '
